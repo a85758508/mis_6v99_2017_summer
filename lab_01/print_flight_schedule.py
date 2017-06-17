746e5ebe2b3272d1dd63d182134fa541a205e4ab
Edison Chen <EDISONCYQ@GMAIL.COM>
2017-06-16 19:35:25 -0500

    ttt

---
+++ lab_01/print_flight_schedule.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+flight_schedule = [['T1','AUS','DAL','0600','0650'],
+ ['T2','DAL','HOU','0600','0705'],
+['T3','DAL','HOU','0600','0705']
+['T4','HOU','AUS','0600','0650'],
+['T5','HOU','DAL','0600','0705']
+['T6','HOU','DAL','0600','0645']
+]
