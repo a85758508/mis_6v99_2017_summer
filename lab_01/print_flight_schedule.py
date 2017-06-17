6c9ddd9511df15c64d633322d375534f7c15decb
Edison Chen <EDISONCYQ@GMAIL.COM>
2017-06-16 19:40:33 -0500

    rr

--- lab_01/print_flight_schedule.py
+++ lab_01/print_flight_schedule.py
@@ -12,3 +12,18 @@ flight_schedule = [['T1','AUS','DAL','0600','0650'],
 ['T5','HOU','DAL','0600','0705']
 ['T6','HOU','DAL','0600','0645']
 ]
+
+csv_header = 'tail_number,origin,destination,departure_time,arrival_time'
+file_name = 'flight_schedule.csv'
+def print_flight_schedule(fn, csv_hdr, flt_sched):
+with open(fn,'wt') as f:
+print(csv_hdr, file=f)
+for s in flt_sched:
+print(','.join(s), file=f)
+# add the flights for T4, T5 and T6 to the list of lists below
+flight_schedule = [['T1','AUS','DAL','0600','0650'],
+['T2','DAL','HOU','0600','0705'],
+['T3','DAL','HOU','0600','0705']]
+
+
+print_flight_schedule(file_name, csv_header, flight_schedule)
